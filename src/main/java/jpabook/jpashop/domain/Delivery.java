bf3a2b9ae8c64ee5c00fa99a7ee014bd82f11633
cotlsdnr@hanmail.net <cotlsdnr@hanmail.net>
2020-02-26 23:51:47 +0900

    1. 지연로딩 추가 2. 연관관계 메서드 추가

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -6,6 +6,8 @@ import sun.rmi.runtime.Log;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter
 @Setter
@@ -16,7 +18,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
     @Embedded
